0df11fd615bd7a27174218e73fbe0846d5b8bd19
abdimalik <abdimalik@gmail.com>
2025-02-06 20:54:49 +0300

    first commit

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
-const Textinput = document.getElementById("text-input")
-const Checkbtn = document.getElementById("check-btn")
-const result = document.getElementById("result")
+const Textinput = document.getElementById("number")
+const Checkbtn = document.getElementById("convert-btn")
+const result = document.getElementById("output")
 
-function isPalindrome(str) {
-    const cleanedStr = str.replace(/[\W_]/g,'').toLowerCase();
-    const reversedStr = cleanedStr.split('').reverse().join('')
-    return cleanedStr === reversedStr
-}
+
 
 Checkbtn.addEventListener('click', ()=>{
-    const inputValue = Textinput.value
-    if (inputValue === '') {
-        alert('Please input a value')
+    const inputValue = +Textinput.value
+    if (!inputValue) {
+        result.classList.add("outputChanger")
+       result.innerText = 'Please enter a valid number'
+  
         return
     }
-    if( isPalindrome(inputValue) ){
-        result.innerText = `${inputValue} is a Palindrome`
-    } else{
-        result.innerText = `${inputValue} is not a Palindrome`
+    if (inputValue < 0) {
+        result.classList.add("outputChanger")
+       result.innerText = 'Please enter a number greater than or equal to 1'
+  
+        return
     }
+    if(inputValue >= 4000){
+        result.classList.add("outputChanger")
+        result.innerText = "Please enter a number less than or equal to 3999"
+        return
+    }
+
+    const lastResult = ConvertNumToRoman(inputValue)
+    result.classList.add("outputChanger")
+    result.innerText = lastResult
 })
+
+
+function ConvertNumToRoman(num){
+    const val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    const syms = ["M","CM", "D","CD","C","XC", "L", "XL","X","IX", "V", "IV", "I"]
+    let Roman = '';
+    let i = 0
+    while (num > 0) {
+        const div = Math.floor(num / val[i])
+        num -= div * val[i]
+        Roman += syms[i].repeat(div)
+        i++
+    }
+    return Roman;
+}   
+
